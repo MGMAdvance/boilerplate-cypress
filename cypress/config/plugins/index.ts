7a5f7857a983d965d29ea34af2fd16a4b1af16b0
Jonathan Daflon <jonathandaflon@yahoo.com>
2021-09-18 22:01:13 -0300

    implementação .env
    Co-authored-by: Andrey Oliveira <andreyoli@users.noreply.github.com>
    Co-authored-by: Matheus Gonçalves <mgmadvance@hotmail.com>

--- cypress/config/plugins/index.ts
+++ cypress/config/plugins/index.ts
@@ -1,18 +1,27 @@
+import * as dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
+const dotenvPlugin = require('cypress-dotenv')
+
 import cucumber from "cypress-cucumber-preprocessor"
-// const report = require("multiple-cucumber-html-reporter")
 import * as browserify from '@cypress/browserify-preprocessor'
 import * as resolve from 'resolve'
+// const report = require("multiple-cucumber-html-reporter")
 
 /**
  * @type {Cypress.PluginConfig}
  */
 
 module.exports = (on, config) => {
+  config = dotenvPlugin(config)
+
   const options = {
     ...browserify.defaultOptions,
     typescript: resolve.sync('typescript', { basedir: config.projectRoot }),
   }
+
   options.browserifyOptions.plugin.unshift(['tsify', { project: config.projectRoot }]);
 
   on('file:preprocessor', cucumber(options))
+
+  return config
 }
